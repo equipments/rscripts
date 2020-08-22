98acd0e811ea3e2fc05dcc820f52ad6b84fce6d7
natashac <natashache3@hotmail.com>
2020-08-22 13:30:54 -0700

    add script

---
+++ Untitled.R
@@ -0,0 +1,14 @@
+C<-matrix(c(15660,21890,129,2883),byrow=T,nrow=2)
+C
+chisq.test(C)
+fisher.test(C)
+
+D<-matrix(c(37274,276,3012,0),byrow=T,nrow=2)
+D
+chisq.test(D)
+fisher.test(D)
+
+E<-matrix(c(34961,2589,2988,24),byrow=T,nrow=2)
+E
+chisq.test(E)
+fisher.test(E)
